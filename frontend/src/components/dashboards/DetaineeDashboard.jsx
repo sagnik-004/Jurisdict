e9ed5ef832c4c8c8f9da7aa28303629365259e98
sagnik-004 <sagraha315@gmail.com>
2025-02-23 05:21:02 +0530

    name update

--- frontend/src/components/dashboards/DetaineeDashboard.jsx
+++ frontend/src/components/dashboards/DetaineeDashboard.jsx
@@ -178,7 +178,7 @@ const DetaineeDashboard = () => {
                   color: themeColors.text,
                 }}
               >
-                Themisync
+                JurisDict
               </h1>
             </div>
           </div>
