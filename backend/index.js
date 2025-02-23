4688856a7988b235b76ac96c8e41c707fb72c907
Sagnik Raha <69630418+sagnik-004@users.noreply.github.com>
2025-02-23 05:56:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",  
+    origin: "jurisdict.pages.dev",  
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'], 
