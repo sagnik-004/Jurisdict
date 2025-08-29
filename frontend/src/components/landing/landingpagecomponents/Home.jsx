29f9a655874d8b388bdda38992ef9cefb8912f3a
sourish-007 <sourishsarkar0011@gmail.com>
2025-08-29 22:56:31 +0530

    casing fixed for typewriter file

--- frontend/src/components/landing/landingpagecomponents/Home.jsx
+++ frontend/src/components/landing/landingpagecomponents/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import TypeWriter from "./TypeWriter";
+import TypeWriter from "./Typewriter";
 
 const Home = ({ darkMode }) => {
   return (
